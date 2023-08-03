bf54d623257cb74aa7bdadc24ccbe28f9f52a2a5
Mthage20 <90241683+Mthage20@users.noreply.github.com>
2023-08-03 20:35:34 +0200

    Initial commit with utmost basic functionality

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using MySqlConnector;
+
+class Program
+{
+    private static readonly string connectionString = "Server=localhost;Database=TicketSystem;User=root;Password=;";
+
+    static void Main()
+    {
+        try
+        {
+            using (var connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+                Console.WriteLine("Connection to MySQL database successful!");
+
+                CreateTicketsTable(connection);
+
+                connection.Close();
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex}");
+            // Optionally, log the error to a file or centralized logging system.
+        }
+    }
+
+    static void CreateTicketsTable(MySqlConnection connection)
+    {
+        string createTableQuery = @"
+            CREATE TABLE IF NOT EXISTS Tickets (
+                Id INT AUTO_INCREMENT PRIMARY KEY
+            )";
+
+        using (var createTableCommand = new MySqlCommand(createTableQuery, connection))
+        {
+            createTableCommand.ExecuteNonQuery();
+        }
+    }
+}
